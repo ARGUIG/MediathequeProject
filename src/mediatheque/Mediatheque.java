4679ebed9e5845a0705ef8c1c5fd949f6026298b
ARGUIG AMINE <amine.arguig@enim.ac.ma>
2024-01-01 12:28:29 +0100

    First Commit Bismilah

--- src/mediatheque/Mediatheque.java
+++ src/mediatheque/Mediatheque.java
@@ -4,7 +4,9 @@ package mediatheque;
 public class Mediatheque {
 
     public static void main(String[] args) {
+        System.out.println("hello Mediatheque");        
         System.out.println("hello Mediatheque");
+
     }
     
 }
